a7d02a4da8dac7a4af871a892b65b7a7f34b7f06
shonac54 <shonac369@gmail.com>
2023-11-18 00:57:31 +0530

    blog header

---
+++ src/components/BlogHeader.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const BlogHeader = () => {
+  return (
+    <div>
+        <nav class="navbar navbar-expand-lg bg-info">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Blog App</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="/">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/add">Add Blog</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/view">View Blog</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/viewmyblog">View MyBlog</a>
+                            </li>
+                                                      
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+    </div>
+  )
+}
+
+export default BlogHeader
--- src/components/ViewBlog.jsx
+++ src/components/ViewBlog.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import BlogHeader from './BlogHeader'
 
 const ViewBlog = () => {
     const [data, changeData] = useState([])
@@ -12,11 +13,12 @@ const ViewBlog = () => {
         )
     }
     const [isLoading, changeLoading] = useState(true)
-    
+
     useEffect(()=>{fetchData()},[])
 
   return (
     <div>
+        <BlogHeader/>
         <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
